d172e3768abec6ffdbfff4ee25731c4571881cde
Yashsingh045 <astomar6396@gmail.com>
2026-02-21 12:00:00 +0530

    chore: Update copyright year to 2026, remove footer image filter, and refine CSS formatting for shadows and keyframes.

--- src/pages/home/Footer.jsx
+++ src/pages/home/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
         <div >
           <img src="./images/logo1.jpeg" alt="Logo" style={{ borderRadius: "30%", marginLeft: "30px" }} />
         </div>
-        <p className="footer--content">Created By Yash | © 2025 All Rights Reserved.™</p>
+        <p className="footer--content">Created By Yash | © 2026 All Rights Reserved.™</p>
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px" }}>
           <a href="https://codeforces.com/profile/YashSingh_045">
             <img src="./images/codeforces.png" alt="Codeforces" style={{ borderRadius: "50%" }} />
